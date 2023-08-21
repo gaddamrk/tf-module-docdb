075e0ce8fe2754da023da7c1db0e868afaa1ea1f
gaddamrk <raj>
2023-08-21 20:26:37 +0530

    jr

--- main.tf
+++ main.tf
@@ -45,6 +45,9 @@ resource "aws_docdb_cluster" "docdb" {
   skip_final_snapshot     = true
   db_subnet_group_name = aws_db_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.docdb.id]
+  storage_encrypted = true
+  kms_key_id = data.aws_kms_key.key.arn
+
 
   tags       = merge(
     local.common_tags,
@@ -57,6 +60,10 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   identifier         = "${var.env}-docdb-cluster-${count.index + 1}"
   cluster_identifier = aws_docdb_cluster.docdb.id
   instance_class     = var.instance_class
+  storage_encrypted = true
+  kms_key_id = data.aws_kms_key.key.arn
+
+
   tags       = merge(
     local.common_tags,
     { Name = "${var.env}-docdb-cluster-instance-${count.index + 1}" }
