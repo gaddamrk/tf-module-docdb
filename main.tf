7e35e417616367ca683275ca9b3a8982dbb53cb9
gaddamrk <raj>
2023-08-28 21:06:35 +0530

    jr

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_docdb_cluster" "docdb" {
   master_username         = data.aws_ssm_parameter.DB_ADMIN_USER.value
   master_password         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
   skip_final_snapshot     = true
-  db_subnet_group_name = aws_db_subnet_group.default.name
+  db_subnet_group_name = aws_docdb_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.docdb.id]
   storage_encrypted = true
   kms_key_id = data.aws_kms_key.key.arn
