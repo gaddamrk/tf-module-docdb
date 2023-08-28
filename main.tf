b85c3a873073b7565ad60ea4844f9da66e51a914
gaddamrk <raj>
2023-08-28 13:00:44 +0530

    jr

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_db_subnet_group" "default" {
   name       = "${var.env}-docdb-subnet-group"
   subnet_ids =  var.subnet_ids
-  availability_zone = var.availability_zone
 
 
   tags       = merge(
